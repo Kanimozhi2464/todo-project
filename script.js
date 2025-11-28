4291c3be0ba64b579b5ca224e91180b308ad17ed
Kanimozhi <kanimozhi2464j@gmail.com>
2025-11-28 20:01:29 +0530

    First todo project

---
+++ script.js
@@ -0,0 +1,29 @@
+let taskInput = document.getElementById("taskInput");
+let addBtn = document.getElementById("addBtn");
+let taskList = document.getElementById("taskList");
+
+addBtn.addEventListener("click", addTask);
+
+function addTask() {
+    let taskText = taskInput.value;
+
+    if (taskText === "") {
+        alert("Please enter a task!");
+        return;
+    }
+
+    let li = document.createElement("li");
+
+    li.innerHTML = `
+        ${taskText}
+        <button class="deleteBtn">Delete</button>
+    `;
+
+    taskList.appendChild(li);
+
+    taskInput.value = ""; // Clear input
+
+    li.querySelector(".deleteBtn").addEventListener("click", function () {
+        li.remove();  // delete the task
+    });
+}
